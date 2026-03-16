cf5d664a2f7f565c3edd35cec0ad1fca3082b9d5
AgataSalika <erkaeva-agata@mail.ru>
2026-03-16 20:17:04 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,36 @@ const ThemeContext = createContext();
 
 function App() {
 
-  const [theme, setTheme] = useState('light')
-  let [textBtn, setTextBtn] = useState('Сменить на темную тему')
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')// начальным значнием для нашей переменной будет
+  //  theme из локал сторадж (если оно там есть), если нет - то будет light
+  let [textBtn, setTextBtn] = useState(theme === 'dark'? 'Сменить на светлую тему' : 'Сменить на темную тему')
   function changeTheme() {
     if (theme === 'light') {
       setTheme('dark')
-     setTextBtn('Сменить на светлую тему')
+      localStorage.setItem('theme', 'dark')
+      setTextBtn('Сменить на светлую тему')
     }
     else {
       setTheme('light')
+      localStorage.setItem('theme', 'light')
       setTextBtn('Сменить на темную тему')
     }
   }
 
-  const [data, setData] = useState(['string1', 'string2', 'string3'])
-
+  const [data, setData] = useState(['Остановись, мгновенье! Ты прекрасно.'])
+  // const numbers = [1,56,78,4,1335]
+  // localStorage.setItem('info', JSON.stringify(numbers)) // сначала указываем название, а потом - то что записываем, записать объект или массив 
+  //  в storage БЕЗ ПОТЕРИ ДАННЫХ JSON.STRINGIFY
+  // let info = JSON.parse(localStorage.getItem('info')) //чтобы ту строку кот там была обратно преобразовать в массив или объект - JSON.PARSE
 
   return (
     <div className={'App ' + theme}>
     <div className="container">
-      <button onClick={changeTheme}>{textBtn}</button>
+      <button className="changeTheme" onClick={changeTheme}>{textBtn}</button>
+       <Form data={data} setData={setData}/>
     </div>
 
-    <Form data={data} setData={setData}/>
+   
     <div>
  
     </div>
--- src/components/TextItem.jsx
+++ src/components/TextItem.jsx
@@ -2,16 +2,33 @@ import React from "react";
 import { useState } from "react";
 
 const TextItem = ({ el }) => {
-  const [fontSize, setFontSize] = useState(16);
-  const [fontColor, setFontColor] = useState("green");
+  const [fontSize, setFontSize] = useState(18);
+  const [fontColor, setFontColor] = useState("#004d40");
   const [fontWeight, setFontWeight] = useState(400);
-  const [backgroundColor, setBackgroundColor] = useState('black')
+  const [makeItalic, setMakeItalic] = useState('normal')
+  const [underline, setUnderline] = useState('auto')
+  const [backgroundColor, setBackgroundColor] = useState('#ffffff')
+  const [textAlign, setTextAlign] = useState('left')
 
   function changeFontWeight() {
     if (fontWeight === 400) {
       setFontWeight(800);
     } else {
       setFontWeight(400);
+    }
+  }
+  function changeItalic() {
+    if(makeItalic === 'normal') {
+      setMakeItalic('italic')
+    } else {
+      setMakeItalic('normal')
+    }
+  }
+  function makeUnderline(){
+      if(underline === 'auto') {
+     setUnderline('underline')
+    } else {
+      setUnderline('auto')
     }
   }
   function incrementSize() {
@@ -22,16 +39,21 @@ const TextItem = ({ el }) => {
       setFontSize(fontSize - 2);
     }
   }
+  
+  const styles = {
+          fontSize: fontSize + "px",
+          color: fontColor,
+          fontWeight: fontWeight,
+          textDecoration: underline,
+          backgroundColor: backgroundColor,
+          textAlign:textAlign,
+          fontStyle:makeItalic
+        }
   return (
     <div className="note-container">
       <p
         className="font-p"
-        style={{
-          fontSize: fontSize + "px",
-          color: fontColor,
-          fontWeight: fontWeight,
-          backgroundColor: backgroundColor,
-        }}
+        style={styles}
       >
         {el}
       </p>
@@ -65,11 +87,27 @@ const TextItem = ({ el }) => {
           ></input>
         </div>
 
-        <div>
-          <button onClick={changeFontWeight}>
-            B
-            {/* <img src="../images/bold-solid-full.svg" alt="bold-icon"></img> */}
+        <div className="icon-flex">
+          <button className="icon" onClick={changeFontWeight}>
+            <img src="/images/bold-solid-full.svg" alt="bold-icon"></img>
           </button>
+           <button className="icon" onClick={changeItalic}>
+            <img src="/images/italic-solid-full.svg" alt="italic-icon"></img>
+          </button>
+           <button className="icon" onClick={makeUnderline}>
+            <img src="/images/underline-solid-full.svg" alt="underline-icon"></img>
+          </button>
+        </div>
+
+        <div className="size-flex">
+          <p className="note-p">Центрировать текст </p>
+          <select onChange={function(event) {
+            setTextAlign(event.target.value)
+          }} value={textAlign}>
+            <option value='left'>по левой стороне</option>
+            <option value='center'>по центру</option>
+            <option value='right'>по правой стороне</option>
+          </select>
         </div>
       </div>
     </div>
